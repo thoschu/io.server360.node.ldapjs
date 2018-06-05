e0b4b8a891549d66b01f976c5f32578783c94d62
Tom S <thoschulte@gmail.com>
2018-06-05 14:03:12 +0200

    update

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-var ldap = require('ldapjs');
+const fs = require('fs');
+const ldap = require('ldapjs');
 
-var server = ldap.createServer();
+const server = ldap.createServer();
 
-function authorize(req, res, next) {
-    if (!req.connection.ldap.bindDN.equals('cn=root'))
-        return next(new ldap.InsufficientAccessRightsError());
+(() => {
+    function authorize(req, res, next) {
+        console.log(req.connection.ldap.bindDN.toString());
+        if (req.connection.ldap.bindDN.equals('cn=root'))
+            return next(new ldap.InsufficientAccessRightsError());
 
-    return next();
-}
+        return next();
+    }
 
-server.search('o=example', authorize, function(req, res, next) {
+    server.bind('cn=root', (req, res, next) => {
+        console.log(req.credentials);
+        console.log(req.dn.toString());
+        if (req.dn.toString() !== 'cn=root' || req.credentials !== 'secret')
+            return next(new ldap.InvalidCredentialsError());
 
-});
+        res.end();
+        return next();
+    });
 
-server.search('o=example', function (req, res, next) {
-
-    console.log(req);
-
-    var obj = {
-        dn: req.dn.toString(),
-        attributes: {
-            objectclass: ['organization', 'top'],
-            o: 'example'
-        }
-    };
-
-    if (req.filter.matches(obj.attributes))
-        res.send(obj);
-    else
-        res.send(obj);
-
-    res.end();
-});
-
-server.listen(10888, '127.0.0.1', function () {
-    console.log('LDAP server listening at %s', server.url);
-});
+    server.listen(10389, '127.0.0.1', () => {
+        console.log('LDAP server listening at %s', server.url);
+    });
+})();
